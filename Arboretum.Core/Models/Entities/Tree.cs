795dba43128e9f3d8b666c678a7aa3928ef2db69
Tomáš Svatek <svatektomas117@gmail.com>
2018-06-04 23:08:35 +0200

    Extending a tree entity

--- Arboretum.Core/Models/Entities/Tree.cs
+++ Arboretum.Core/Models/Entities/Tree.cs
@@ -12,6 +12,10 @@ namespace Arboretum.Core.Models.Entities
         public string SpeciesCommonName { get; set; }
         [JsonProperty( "coordinates" )]
         public LatLng LatLng { get; set; }
+        public int Age { get; set; }    
+        public double Height { get; set; }
+        public double TrunkSize { get; set; }
+        public double CrownSize { get; set; }
         public string About { get; set; }
         public byte[] Image { get; set; }
     }
